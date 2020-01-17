8ea58b75a25405382c96c71b5a50b6c904dd1ccd
sachinchouksey017 <sachinchouksey017@gmail.com>
2020-01-17 11:00:52 +0530

    Refacter

--- arithmaticComputation.sh
+++ arithmaticComputation.sh
@@ -3,13 +3,17 @@
 read -p "enter 1st input " number1
 read -p "enter 2nd input " number2
 read -p "enter 3rd input " number3
-
 if [ $number1 -ge 0 -a $number2 -ge 0 -a $number3 -ge 0 ]
 then
    #UC-2
    result1=$(( number1+number2*number3 ))
    #UC-3
    result2=$(( number1*number2+number3 ))
+   #UC-4
+   result3=$(echo "scale=2;$number3+$number1/$number2" | bc)
+ #UC-4
+   result3=$(echo "scale=2;$number3+$number1/$number2" | bc)
+
 else
    echo "please enter valid input"
 fi
